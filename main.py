07b9f4b6fdd22d33d120cb4424b9149454d80ee9
EC2 Default User <ec2-user@ip-172-31-0-48.ec2.internal>
2020-09-23 21:39:57 +0000

    Add randomized images to index.html

--- main.py
+++ main.py
@@ -3,10 +3,18 @@ import os
 import random
 
 app = flask.Flask(__name__)
+picturelst=[
+    "NJIT Logo.png",
+    "Eberhardt Hall.JPG"
+]
 
 @app.route('/') # Python decorator
 def index():
-    return flask.render_template("index.html")
+    return flask.render_template(
+        "index.html",
+        pictures_lst=picturelst,
+        random_index=random.randrange(0,len(picturelst))
+    )
     
 app.run(
     port=int(os.getenv('PORT', 8080)),
